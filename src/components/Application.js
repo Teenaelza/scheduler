e333a87d0ecb41d1f1567589a3062c890a242ca9
Teenaelza <teena.thaliath@gmail.com>
2021-10-01 19:56:53 -0700

    Retreviewing interviwer data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment/index";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 // const appointments = [
 //   {
 //     id: 1,
@@ -54,26 +54,37 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+
     appointments: {},
+    interviewers: {},
   });
+  //get appointement array for the day selected
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = (day) => setState({ ...state, day });
-  const setDays = (days) => setState({ ...state, days });
-
-  const appointmentArray = dailyAppointments.map((appointment) => (
-    <Appointment key={appointment.id} {...appointment} />
-  ));
+  //create an appointment component  for each appointment
+  const appointmentArray = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+        interview={interview}
+      />
+    );
+  });
   const URL1 = "/api/days";
   const URL2 = "/api/appointments";
+  const URL3 = "/api/interviewers";
   useEffect(() => {
-    Promise.all([axios.get(URL1), axios.get(URL2)]).then((all) => {
-      const [first, second] = all;
-      const days = first.data;
-      const appointments = second.data;
-      console.log(appointments);
-      setState((prev) => ({ ...prev, days, appointments }));
-    });
+    Promise.all([axios.get(URL1), axios.get(URL2), axios.get(URL3)]).then(
+      (all) => {
+        const [first, second, third] = all;
+        const days = first.data;
+        const appointments = second.data;
+        const interviewers = third.data;
+        setState((prev) => ({ ...prev, days, appointments, interviewers }));
+      }
+    );
   }, []);
 
   return (
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -7,3 +7,13 @@ export function getAppointmentsForDay(state, day) {
   }
   return [];
 }
+
+export function getInterview(state, interview) {
+  if (interview !== null) {
+    return {
+      interviewer: { ...state.interviewers[interview.interviewer] },
+      student: interview.student,
+    };
+  }
+  return null;
+}
